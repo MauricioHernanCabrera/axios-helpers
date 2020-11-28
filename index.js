f92a07c79f0354ea1c98976091efe4c7b5f16fd0
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2020-11-28 15:43:35 -0300

    async fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const axiosData = (axios, httpMethod) => (params) => {
-  const response = await axios[httpMethod](params);
+const axiosData = (axios, httpMethod) => async (...params) => {
+  const response = await axios[httpMethod](...params);
   return response.data;
 };
 
@@ -13,23 +13,23 @@ const setBaseUrl = (axios) => (baseURL) => {
 
 const setToken = (axios) => (token, type = null) => {
   switch (type) {
-    case 'Paymentez':
-      return axios.defaults.headers.common['Auth-Token'] = token
-    
-    case 'Bearer':
-      return axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-        
-    default: 
-      return axios.defaults.headers.common['Authorization'] = token
+    case "Paymentez":
+      return (axios.defaults.headers.common["Auth-Token"] = token);
+
+    case "Bearer":
+      return (axios.defaults.headers.common["Authorization"] = `Bearer ${token}`);
+
+    default:
+      return (axios.defaults.headers.common["Authorization"] = token);
   }
 };
 
 const axiosHelpers = (axios) => {
-  const httpMethods = ["post", "get", "delete", "patch", "put"]
+  const httpMethods = ["post", "get", "delete", "patch", "put"];
 
   httpMethods.forEach((httpMethod) => {
-    axios.$[httpMethod] = axiosData(httpMethod)
-  })
+    axios.$[httpMethod] = axiosData(httpMethod);
+  });
 
   axios.setHeader = setHeader(axios);
   axios.setBaseUrl = setBaseUrl(axios);
