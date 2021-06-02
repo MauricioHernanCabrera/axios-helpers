3045692f25777a8e14f2612fbb83110907de2fe9
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2021-06-02 11:13:14 -0300

    basic auth added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const axiosData = (axios, httpMethod) => async (...params) => {
-  try {
-    const response = await axios[httpMethod](...params);
-    return Promise.resolve(response.data);
-  } catch (error) {
-    return Promise.reject(error.response.data);
-  }
-};
+const axiosData =
+  (axios, httpMethod) =>
+  async (...params) => {
+    try {
+      const response = await axios[httpMethod](...params);
+      return Promise.resolve(response.data);
+    } catch (error) {
+      return Promise.reject(error.response.data);
+    }
+  };
 
 const setHeader = (axios) => (property, data) => {
   axios.defaults.headers.common[property] = data;
@@ -15,18 +17,30 @@ const setBaseURL = (axios) => (baseURL) => {
   axios.defaults.baseURL = baseURL;
 };
 
-const setToken = (axios) => (token, type = null) => {
-  switch (type) {
-    case "Paymentez":
-      return (axios.defaults.headers.common["Auth-Token"] = token);
+const setToken =
+  (axios) =>
+  (token, type = null) => {
+    switch (type) {
+      case "Paymentez": {
+        return (axios.defaults.headers.common["Auth-Token"] = token);
+      }
 
-    case "Bearer":
-      return (axios.defaults.headers.common["Authorization"] = `Bearer ${token}`);
+      case "Bearer": {
+        return (axios.defaults.headers.common["Authorization"] = `Bearer ${token}`);
+      }
 
-    default:
-      return (axios.defaults.headers.common["Authorization"] = token);
-  }
-};
+      case "Basic": {
+        const { user, pass } = token;
+        return (axios.defaults.headers.common["Authorization"] = `Basic ${Buffer.from(
+          `${user}:${pass}`
+        ).toString("base64")}`);
+      }
+
+      default: {
+        return (axios.defaults.headers.common["Authorization"] = token);
+      }
+    }
+  };
 
 const axiosHelpers = (axios) => {
   const httpMethods = ["post", "get", "delete", "patch", "put"];
